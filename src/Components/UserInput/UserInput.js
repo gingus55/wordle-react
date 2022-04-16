c8b1af7320c1bbf65edb28cf88d15fae8980d45f
gingus55 <gingus55@hotmail.com>
2022-04-16 20:47:04 +0100

    console logging input handled

--- src/Components/UserInput/UserInput.js
+++ src/Components/UserInput/UserInput.js
@@ -1,4 +1,5 @@
 import { Button, FormControl, Input, InputLabel } from "@mui/material";
+import { createEvent } from "@testing-library/dom";
 import React from "react";
 import "./userInput.css";
 
@@ -7,12 +8,17 @@ const handleSubmit = (event) => {
   console.log("submitted");
 };
 
+const handleValue = (event) => {
+  event.preventDefault();
+  console.log(event.target.value);
+};
+
 export const UserInput = () => {
   return (
     <FormControl className="guessForm">
       <InputLabel>Guess:</InputLabel>
 
-      <Input id="guess-input" type="text" name="guess" />
+      <Input onChange={handleValue} id="guess-input" type="text" name="guess" />
       <Button onClick={handleSubmit} type="submit" value="submit">
         SUBMIT
       </Button>
