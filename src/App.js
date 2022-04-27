8db39ea8bbaf94f8cb1e945722ce0e3eb1c2d608
gingus55 <gingus55@hotmail.com>
2022-04-27 18:16:40 +0100

    more clg removed

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import { Title } from "./Components/Title/title";
 import { wordArray } from "./GameLogic/logic";
 
 export const App = () => {
-  console.log(wordArray);
-
   const guesses = ["guess1", "guess2", "guess3", "guess4", "guess5", "guess6"];
 
   return (
